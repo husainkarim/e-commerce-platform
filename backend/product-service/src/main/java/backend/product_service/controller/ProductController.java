ea4f13865e526f4f6eb1bc05b7e2106db34ce532
halabboo <husain.akarim@gmail.com>
2025-10-22 21:15:08 +0300

    add planed product service handler

---
+++ backend/product-service/src/main/java/backend/product_service/controller/ProductController.java
@@ -0,0 +1,14 @@
+package backend.product_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    // product list
+    // product details
+    // create product
+    // update product
+    // delete product
+}
