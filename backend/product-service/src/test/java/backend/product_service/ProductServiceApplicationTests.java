71f6d637eeeaa6606241eb7165763272d47727b8
halabboo <husain.akarim@gmail.com>
2025-12-08 19:29:26 +0300

    update

--- backend/product-service/src/test/java/backend/product_service/ProductServiceApplicationTests.java
+++ backend/product-service/src/test/java/backend/product_service/ProductServiceApplicationTests.java
@@ -2,8 +2,12 @@ package backend.product_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.kafka.bootstrap-servers=" 
+})
 class ProductServiceApplicationTests {
 
 	@Test
