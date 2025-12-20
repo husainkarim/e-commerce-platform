83ae7de1e02f70b8c89854eb352d7fa3ed61f1ef
halabboo <husain.akarim@gmail.com>
2025-12-20 23:41:49 +0300

    update

--- backend/user-service/src/test/java/backend/user_service/UserServiceApplicationTests.java
+++ backend/user-service/src/test/java/backend/user_service/UserServiceApplicationTests.java
@@ -3,10 +3,19 @@ package backend.user_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import backend.user_service.repository.UserRepository;
+import backend.user_service.service.KafkaService;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
 class UserServiceApplicationTests {
+
+	@MockBean
+    private UserRepository userRepository;
+
+	@MockBean
+	private KafkaService kafkaService;
 
     @Test
     void contextLoads() {
