8b32f5f8b15b0c50aba077c4dab08108315cd143
halabboo <husain.akarim@gmail.com>
2025-12-22 21:10:41 +0300

    update

--- backend/media-service/src/test/java/backend/media_service/config/FirebaseTestConfig.java
+++ backend/media-service/src/test/java/backend/media_service/config/FirebaseTestConfig.java
@@ -3,13 +3,14 @@ package backend.media_service.config;
 import static org.mockito.Mockito.mock;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 import com.google.firebase.FirebaseApp;
-
 @TestConfiguration
 public class FirebaseTestConfig {
 
     @Bean
+    @Primary
     public FirebaseApp firebaseApp() {
         // Return a mock instead of real FirebaseApp
         return mock(FirebaseApp.class);
