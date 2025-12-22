7f6db5188d410beaa35324a7050a6bb7c3585aa7
halabboo <husain.akarim@gmail.com>
2025-12-22 21:40:25 +0300

    update

--- backend/media-service/src/test/java/backend/media_service/MediaServiceApplicationTests.java
+++ backend/media-service/src/test/java/backend/media_service/MediaServiceApplicationTests.java
@@ -8,12 +8,25 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import backend.media_service.config.FirebaseTestConfig;
+import backend.media_service.config.FileStorageTestConfig;
+
+import backend.media_service.service.FileStorageService;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import backend.media_service.config.FirebaseConfig;
 
 @SpringBootTest
 @EnableAutoConfiguration(exclude = {KafkaAutoConfiguration.class})
-@Import(FirebaseTestConfig.class)
+@Import(value = {FirebaseTestConfig.class, FileStorageTestConfig.class})
 @ActiveProfiles("test")
 class MediaServiceApplicationTests {
+
+	@MockBean
+    private FileStorageService fileStorageService;
+
+	@MockBean
+	private FirebaseConfig firebaseConfig;
 
 	@Test
 	void contextLoads() {
